515c463af7791aaa2d67b7dbbd3857640a68bc78
Christoph Rzymski <christoph@functor-argument.net>
2019-04-08 10:00:05 +0200

    Fixed broken setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,6 @@ with open('metadata.json', **({} if PY2 else {'encoding': 'utf-8'})) as fp:
 
 setup(
     name='lexibank_hubercolumbian',
-    version="1.0",
     description=metadata['title'],
     license=metadata.get('license', ''),
     url=metadata.get('url', ''),
@@ -23,11 +22,6 @@ setup(
         ]
     },
     install_requires=[
-<<<<<<< HEAD
-        'pylexibank>=1.0',
-=======
-        'pylexibank>=0.11',
-        'segments>=2.0.1',
->>>>>>> 01da4f1c356d420bc453397241232c6588f94c26
+        'pylexibank==1.1.1',
     ]
 )
