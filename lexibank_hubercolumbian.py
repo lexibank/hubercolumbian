ea63837c5beafa3610aca0720a19ca94a62ea7b3
Johann-Mattis List <LinguList@users.noreply.github.com>
2019-11-06 09:38:40 +0100

    Refactor hubercolumbian for pylexibank 2.0
    * update
    
    * Code formatting.
    
    * value instead of form?
    
    * Fix problematic coordinates
    
    * Fix concept lookup
    
    * Re-generated CLDF

--- lexibank_hubercolumbian.py
+++ lexibank_hubercolumbian.py
@@ -1,25 +1,32 @@
-from itertools import groupby
 from pathlib import Path
 
 import attr
 import lingpy
 from clldutils.misc import slug
-from pylexibank import Concept
+from pylexibank import Concept, Language
 from pylexibank.providers import qlc
 from pylexibank.util import progressbar
 
 
 @attr.s
-class HConcept(Concept):
+class CustomConcept(Concept):
     Spanish = attr.ib(default=None)
     Gloss_in_digital_source = attr.ib(default=None)
+
+
+@attr.s
+class CustomLanguage(Language):
+    Longitude = attr.ib(default=None)
+    Latitude = attr.ib(default=None)
+    Name_in_Source = attr.ib(default=None)
 
 
 class Dataset(qlc.QLC):
     dir = Path(__file__).parent
     id = "hubercolumbian"
     DSETS = ["huber1992.csv"]
-    concept_class = HConcept
+    concept_class = CustomConcept
+    language_class = CustomLanguage
 
     def cmd_makecldf(self, args):
         # column "counterpart_doculect" gives us the proper names of the doculects
@@ -27,57 +34,27 @@ class Dataset(qlc.QLC):
         args.writer.add_sources()
 
         # get the language identifiers stored in wl._meta['doculect'] parsed from input file
-        lids = {}
-        for line in wl._meta["doculect"]:
-            rest = line.split(", ")
-            name = rest.pop(0)
-            lids[name] = rest.pop(0)
+        language_lookup = args.writer.add_languages(lookup_factory="Name_in_Source")
 
-        concept_lookup = {}
+        concept_lookup = args.writer.add_concepts(
+            id_factory=lambda x: x.id.split("-")[-1] + "_" + slug(x.english),
+            lookup_factory="Gloss_in_digital_source",
+        )
 
-        for concept in self.conceptlists[0].concepts.values():
-            concept_id = "%s_%s" % (concept.number, slug(concept.english))
-
-            args.writer.add_concept(
-                ID=concept_id,
-                Name=concept.english,
-                Concepticon_ID=concept.concepticon_id,
-                Concepticon_Gloss=concept.concepticon_gloss,
-                Gloss_in_digital_source=concept.attributes["gloss_in_digital_source"],
-                Spanish=concept.attributes["spanish"],
+        rows = [
+            (doculect, concept, value, qlcid)
+            for (idx, doculect, concept, value, qlcid) in wl.iter_rows(
+                "counterpart_doculect", "concept", "counterpart", "qlcid"
             )
+            if doculect not in ["English", "Español"]
+        ]
 
-            concept_lookup[concept.attributes["gloss_in_digital_source"]] = concept_id
-
-        def grouped_rows(wl):
-            rows = [
-                (
-                    wl[k, "counterpart_doculect"],
-                    wl[k, "concept"],
-                    wl[k, "counterpart"],
-                    wl[k, "qlcid"],
-                )
-                for k in wl
-                if wl[k, "counterpart_doculect"] not in ["English", "Español"]
-            ]
-            return groupby(sorted(rows), key=lambda r: (r[0], r[1]))
-
-        for (language, concept), rows in progressbar(grouped_rows(wl)):
-            iso = lids[language]
-
-            args.writer.add_language(
-                ID=slug(language, lowercase=False),
-                Name=language,
-                ISO639P3code=iso,
-                Glottocode=self.glottolog.glottocode_by_iso.get(iso, ""),
+        for doculect, concept, value, qlcid in progressbar(rows):
+            args.writer.add_form(
+                Language_ID=language_lookup[doculect],
+                Parameter_ID=concept_lookup[concept],
+                Value=value,
+                Form=value,
+                Source=["Huber1992"],
+                Local_ID=qlcid,
             )
-
-            for i, (l, c, form, id_) in enumerate(rows):
-                args.writer.add_form(
-                    Language_ID=slug(language, lowercase=False),
-                    Parameter_ID=concept_lookup[concept],
-                    Value=form,
-                    Form=form,
-                    Source=["Huber1992"],
-                    Local_ID=id_,
-                )
